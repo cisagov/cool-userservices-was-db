0c2a21ec206341f6fe7eb954f5ebbbbb7f72d810
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-02-06 16:21:30 -0500

    Add outputs for read-only and read-write roles

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
+output "read_only_role" {
+  description = "The WAS database read-only role."
+  value       = aws_iam_role.read_only
+}
+
+output "read_write_role" {
+  description = "The WAS database read-write role."
+  value       = aws_iam_role.read_write
+}
+
 output "was_db_table" {
   description = "The WAS database table."
   value       = aws_dynamodb_table.was
