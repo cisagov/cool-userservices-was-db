1af2332f5b3b276ed11a68c958201b04c4382c42
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-03-18 14:18:13 -0400

    Add an output for the reports table

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "read_write_role" {
   value       = aws_iam_role.read_write
 }
 
+output "reports_table" {
+  description = "The WAS reports DynamoDB table."
+  value       = var.reports_table_sort_key == "" ? aws_dynamodb_table.reports_without_sort_key[0] : aws_dynamodb_table.reports_with_sort_key[0]
+}
+
 output "stakeholders_table" {
   description = "The WAS stakeholders DynamoDB table."
   value       = var.stakeholders_table_sort_key == "" ? aws_dynamodb_table.stakeholders_without_sort_key[0] : aws_dynamodb_table.stakeholders_with_sort_key[0]
