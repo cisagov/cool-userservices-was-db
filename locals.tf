ae8e2008219ada9a4a54a3fe377f18793ccce968
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2024-03-18 13:58:28 -0400

    Add locals for reports table, alphabetize local variables, and add an explanatory comment

--- locals.tf
+++ locals.tf
@@ -28,6 +28,17 @@ locals {
   # names.
   caller_user_name = split("/", data.aws_caller_identity.current.arn)[1]
 
+  # Since the reports and stakeholders tables may or may not have a sort key,
+  # there are potentially two different Terraform resources for each table
+  # (even though only one will be created).  To account for this, we create
+  # two local variables for each table, one for the ARN and one for the name,
+  # to be used in the IAM policy documents.
+  reports_table_arn  = var.reports_table_sort_key == "" ? aws_dynamodb_table.reports_without_sort_key[0].arn : aws_dynamodb_table.reports_with_sort_key[0].arn
+  reports_table_name = var.reports_table_sort_key == "" ? aws_dynamodb_table.reports_without_sort_key[0].name : aws_dynamodb_table.reports_with_sort_key[0].name
+
+  stakeholders_table_arn  = var.stakeholders_table_sort_key == "" ? aws_dynamodb_table.stakeholders_without_sort_key[0].arn : aws_dynamodb_table.stakeholders_with_sort_key[0].arn
+  stakeholders_table_name = var.stakeholders_table_sort_key == "" ? aws_dynamodb_table.stakeholders_without_sort_key[0].name : aws_dynamodb_table.stakeholders_with_sort_key[0].name
+
   # Find the Users account by name and extract the account ID
   users_account_id = [
     for account in data.aws_organizations_organization.cool.accounts :
@@ -37,7 +48,4 @@ locals {
 
   # The User Services account ID
   userservices_account_id = data.aws_caller_identity.userservices.account_id
-
-  stakeholders_table_arn  = var.stakeholders_table_sort_key == "" ? aws_dynamodb_table.stakeholders_without_sort_key[0].arn : aws_dynamodb_table.stakeholders_with_sort_key[0].arn
-  stakeholders_table_name = var.stakeholders_table_sort_key == "" ? aws_dynamodb_table.stakeholders_without_sort_key[0].name : aws_dynamodb_table.stakeholders_with_sort_key[0].name
 }
